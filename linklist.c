a7e33827172277ca5753df963f573f374a7fc7d5
dcapple <44258998+dcapple@users.noreply.github.com>
2018-10-26 15:08:46 +0800

    Update linklist.c

---
+++ linklist.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+int data;
+struct node *next;
+};
+
+typedef struct node NODE;
+
+NODE *build_link(int *,int );
+void show_list(NODE *);
+NODE *search(NODE *,int);
+void insert_node(NODE *,int);
+NODE *delete_node(NODE *,NODE *);
+NODE *reverse_list(NODE *);
+
+int main(void){
+
+int arr[3]={2,4,1};
+NODE *head;
+head=build_link(arr,3);
+show_list(head);
+head=reverse_list(head);
+show_list(head);
+return 0;
+}
+
+NODE *build_link(int *arr,int len){
+NODE *head,*current,*pre;
+
+for(int i=0;i<len;i++){
+    current=(NODE *)malloc(sizeof(NODE));
+    current->data=arr[i];
+
+if(i==0)
+    head=current;
+else
+    pre->next=current;
+current->next=NULL;
+pre=current;
+}
+return head;
+}
+
+void show_list(NODE *head){
+
+while(head){
+   printf("%d ",head->data);
+    head=head->next;
+}
+}
+
+NODE *search(NODE *head,int item){
+
+while(head->next){
+    if(head->data==item)
+        return head;
+    else
+        head=head->next;
+}
+printf("not found!! \n");
+
+}
+
+void insert_node(NODE *node,int item){
+
+NODE *insert_item;
+insert_item=(NODE *)malloc(sizeof(NODE));
+insert_item->data=item;
+insert_item->next=node->next;
+node->next=insert_item;
+
+}
+
+NODE *delete_node(NODE *head,NODE *node){
+
+NODE *ptr=head;
+
+if(!head)
+    printf("empty! \n");
+
+if(head==node)
+    head=head->next;
+else{
+    while(ptr->next!=node)
+        ptr=ptr->next;
+    ptr->next=node->next;
+}
+free(node);
+
+return head;
+}
+
+NODE *reverse_list(NODE *head){
+
+NODE *temp,*p;
+temp=NULL;
+while(head!=NULL){
+    p=head;
+    head=head->next;
+    p->next=temp;
+    temp=p;
+}
+return p;
+}
