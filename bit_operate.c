ad337271c65c4941da2775edd499ec7b72827093
dcapple <44258998+dcapple@users.noreply.github.com>
2019-03-27 21:42:55 +0800

    Update bit_operate.c

--- bit_operate.c
+++ bit_operate.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define bit3 (0x1<<3)
-void trans(int num,int form,int *bit);
+
 void show(int *);
 
 
@@ -28,55 +28,5 @@ void show(int *bit){
 for(int i=9;i>=0;i--)
     printf("%d",*(bit+i));
 
-
-}
-
-void trans(int num,int form,int *bit){
-
-int i=0;
-switch(form){
-case 2 :
-
-  while( num>=2){
-    *(bit+i)=num%2;
-    num=num/2;
-    i++;
-  }
-  *(bit+i)=num;
-  break;
-
-case 8:
-
-  while( num>=8){
-    *(bit+i)=num%8;
-    num=num/8;
-    i++;
-  }
-  *(bit+i)=num;
-  break;
-
-  case 10:
-
-  while( num>=10){
-    *(bit+i)=num%10;
-    num=num/10;
-    i++;
-  }
-  *(bit+i)=num;
-  break;
-
-case 16:
-
-  while( num>=16){
-    *(bit+i)=num%16;
-    num=num/16;
-    i++;
-  }
-  *(bit+i)=num;
-  break;
-
-default:
-    printf("failed\n");
-}
 
 }
