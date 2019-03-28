9cd97dc36867c7d6d36d60059ec257d8c8a2c29f
dcapple <44258998+dcapple@users.noreply.github.com>
2019-03-28 10:07:07 +0800

    Update duplicatechar.c

--- duplicatechar.c
+++ duplicatechar.c
@@ -4,7 +4,8 @@ int i,j,len=strlen(s),count=0;
 for(i=0;i<len;i++){
    for(j=0;j<len;j++){
 	if(s[i]==s[j])
-		count++;
+		//計算重複次數
+		count++;     
 	if(count>1)
 		printf("%c : %d \n",s[i],count);
 	}
