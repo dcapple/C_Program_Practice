02ea058b8bd6c2688c043542309437f433faed49
dcapple <44258998+dcapple@users.noreply.github.com>
2019-03-28 15:39:27 +0800

    Update twosum.c

--- twosum.c
+++ twosum.c
@@ -1,9 +1,11 @@
 int* twoSum(int* nums, int numsSize, int target) {
     
 int i,j;
-    
+
+//用動態記憶體配置，直接給予一段記憶體位置才有辦法傳副函式外
 int *answer=(int *)malloc(2*sizeof(int));
-    
+
+//簡易的檢查邏輯
 for(i=0;i<(numsSize-1);i++)
     
     for(j=i+1;j<numsSize;j++){
