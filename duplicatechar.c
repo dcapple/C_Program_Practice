2c928c26314919cbddd329c8ffcb930c7cc227e3
dcapple <44258998+dcapple@users.noreply.github.com>
2019-03-28 10:04:01 +0800

    Create duplicatechar.c

---
+++ duplicatechar.c
@@ -0,0 +1,13 @@
+void duplicatechar(char *s){
+int i,j,len=strlen(s),count=0;
+
+for(i=0;i<len;i++){
+   for(j=0;j<len;j++){
+	if(s[i]==s[j])
+		count++;
+	if(count>1)
+		printf("%c : %d \n",s[i],count);
+	}
+	count=0;
+}
+}
