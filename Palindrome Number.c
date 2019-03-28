dd2827a00bde4809c03d7dbbf873d030d4f569e5
dcapple <44258998+dcapple@users.noreply.github.com>
2019-03-28 15:26:59 +0800

    Update Palindrome Number.c

--- Palindrome Number.c
+++ Palindrome Number.c
@@ -1,20 +1,19 @@
 bool isPalindrome(int x) {
     
-
     int i,j,temp=x;
     
     int count=0;
-    
+    //基本判斷
     if(temp<0)
         return false;
     else if(temp<10)
         return true;
-    
+    //計算輸入的整數有幾位數
     while(temp!=0){
         temp/=10;
         count++;
     }
-    
+    //將每位數輸入進陣列中
     int bit[count];
     temp=x;
     for(i=(count-1);i>-1;i--){
@@ -22,7 +21,7 @@ bool isPalindrome(int x) {
         x/=10;
         bit[i]=temp;
     }
-    
+    //檢查是否迴文
     for(i=(count-1),j=0;j<i;j++,i--){
         if(bit[i]!=bit[j])
             return false;
