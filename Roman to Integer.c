39f1338c09c78477dba3214ad7a021d32eb1d20b
dcapple <44258998+dcapple@users.noreply.github.com>
2019-03-28 15:28:51 +0800

    Update Roman to Integer.c

--- Roman to Integer.c
+++ Roman to Integer.c
@@ -2,6 +2,7 @@ int romanToInt(char* s) {
     int i=0,sum=0,len=strlen(s);
     
     int roam[128]={NULL};
+    //先定義出每個羅馬數字代表的整數
     roam['I']=1;
     roam['V']=5;
     roam['X']=10;
@@ -10,7 +11,7 @@ int romanToInt(char* s) {
     roam['D']=500;
     roam['M']=1000;
     
-    
+    //判斷後一個羅馬數字是否大於現在的
     while(i<len){
        if(i+1<len && roam[s[i]]<roam[s[i+1]]){
            sum+=roam[s[i+1]]-roam[s[i]];
